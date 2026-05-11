e1a5cdc716482ef28d4196639b94be22dbd93776
gboone11 <gavin.boone@ejgallo.com>
2026-05-11 14:27:05 -0700

    hex numbers

--- main.c
+++ main.c
@@ -378,7 +378,8 @@ void editorUpdateSyntax(erow* row) {
         }
 
         if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
-            if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)) {
+            if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER) ||
+                (c == 'x' && prev_hl == HL_NUMBER) || (isxdigit(c) && prev_hl == HL_NUMBER)) {
                 row->hl[i] = HL_NUMBER;
                 i++;
                 prev_sep = 0;
